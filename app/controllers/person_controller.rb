f314aabe679eaa1ca7e55fb89571db7fc1b0e9f2
Gianluca Fenzo <gfenzo@miuandes.cl>
2018-05-15 17:46:28 -0400

    fixed some things

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -11,9 +11,27 @@ class PersonController < ApplicationController
 
   def create
     if params[:type] == "Actor"
-      @actor = Actor.new(first_name:params[:first_name], last_name:params[:last_name], birth_date:params[:birth_date], description:params[:description])
+      @actor = Actor.create(first_name:params[:first_name], last_name:params[:last_name], birth_date:params[:birth_date], description:params[:description])
+      respond_to do |format|
+        if @actor.save
+          format.html { redirect_to @actor, notice: 'Actor was successfully created.' }
+          format.json { render :show, status: :created, location: @actor }
+        else
+          format.html { render :new }
+          format.json { render json: @actor.errors, status: :unprocessable_entity }
+        end
+      end
     else
-      @director = Director.new(first_name:params[:first_name], last_name:params[:last_name], birth_date:params[:birth_date], description:params[:description])
+      @director = Director.create(first_name:params[:first_name], last_name:params[:last_name], birth_date:params[:birth_date], description:params[:description])
+      respond_to do |format|
+        if @director.save
+          format.html { redirect_to @director, notice: 'Director was successfully created.' }
+          format.json { render :show, status: :created, location: @director }
+        else
+          format.html { render :new }
+          format.json { render json: @director.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
   end
